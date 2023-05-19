1f965ee382edde618ed28759cd81099e1feead76
Techbrolakes <lekandar11@gmail.com>
2023-05-19 13:01:49 +0100

    Feat: Project Structure Configurations

---
+++ config/constants.ts
@@ -0,0 +1,33 @@
+const API = {
+    baseURL: 'http://localhost:400/api/v1/',
+    error: {
+        aborted: {
+            code: 'ECONNABORTED',
+            description: 'Please check your network connection and try again',
+            message: 'Network Error!',
+        },
+        expiredToken: 'Access denied. Token Expired',
+    },
+    routes: {
+        auth: {
+            login: 'user/login',
+            recoverPassword: 'user/recover',
+            registerUser: 'user/register',
+            resendVerification: 'user/resend',
+            resetPassword: 'user/reset-password',
+            verifyOtp: '/user/verify-otp',
+        },
+        userManagement: {
+            editUser: 'user/edit',
+            getUserDetails: 'user/get',
+            resetPassword: 'user/reset',
+        },
+    },
+    timeout: 500000,
+};
+
+const constants = {
+    API,
+};
+
+export default constants;
