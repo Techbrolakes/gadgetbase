d1b4d1c9c833898d344d37937110b9022dea9fd4
Techbrolakes <lekandar11@gmail.com>
2023-05-27 13:48:21 +0100

    Feat: Product Crud Wrapping up Configs

--- components/pagecomponents/admin/products/ProductsSection.tsx
+++ components/pagecomponents/admin/products/ProductsSection.tsx
@@ -4,12 +4,24 @@ import Input from '@/components/common/components/Input';
 import useGetProducts from '@/hooks/products/useGetProducts';
 import ProductsTable from './ProductsTable';
 import AddProduct from './AddProduct';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react';
 
 const ProductsSection: React.FC = () => {
     const [isOpen, setIsOpen] = React.useState(false);
+    const [openModal, setOpenModal] = React.useState(false);
     const [search, setSearch] = useState('');
     const { data, isLoading } = useGetProducts({ search });
     const handleOpen = () => setIsOpen(true);
+    const onClose = () => setOpenModal(false);
+    const handleModelOpen = () => setOpenModal(true);
 
     return (
         <section className="mt-12 space-y-12">
@@ -18,10 +30,19 @@ const ProductsSection: React.FC = () => {
                     <Input placeholder="Search Products" value={search} onChange={(e) => setSearch(e.target.value)} />
                 </div>
                 <Button name="Add Product" onClick={() => handleOpen()} />
-                <Button name="Filter Product" onClick={() => handleOpen()} />
+                <Button name="Filter Product" onClick={() => handleModelOpen()} />
             </div>
             <ProductsTable isLoading={isLoading} data={data?.data?.data} />
             <AddProduct isOpen={isOpen} setIsOpen={setIsOpen} />
+
+            <Modal isOpen={openModal} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader textStyle="p">Filter Products</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody></ModalBody>
+                </ModalContent>
+            </Modal>
         </section>
     );
 };
